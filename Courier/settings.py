47edb1527177296cbf7967578f828a14a5dd6ad7
Rupesh Sigdel <rupeshcgdl2060@gmail.com>
2024-12-17 20:57:41 +0545

    creating .env file

--- Courier/settings.py
+++ Courier/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Use the variables
+GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
